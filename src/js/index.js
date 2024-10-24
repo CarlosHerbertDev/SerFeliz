da05a53b2550f5ce99654914b7769809dca01388
CarlosHerbertDev <hrbrtvieira@gmail.com>
2024-10-24 16:30:30 -0300

    dividindo css

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,12 @@
 const gallery = document.getElementById('animated-thumbnails')
 const lastOne = document.querySelectorAll('.last-one')
+
 const myObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting){
             entry.target.classList.add("animate__fadeIn")
-        } else {
-            entry.target.classList.remove("animate__fadeIn")
-        }
+        } 
     })
-
-
-
-
-  
-    
 })
 
 lastOne.forEach((item) => {
