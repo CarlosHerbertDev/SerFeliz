8d50f25f16309f66502e5e55795ded5efd8237c8
CarlosHerbertDev <hrbrtvieira@gmail.com>
2024-10-25 09:30:30 -0300

    header fixo

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,28 @@
 const gallery = document.getElementById('animated-thumbnails')
 const lastOne = document.querySelectorAll('.last-one')
+const nav = document.querySelector(".header")
+
+let prevScrollops = window.pageYOffset;
+
+window.addEventListener('scroll', () => {
+
+    
+    
+    let currentScrollops = window.pageYOffset
+        
+
+    if (prevScrollops < currentScrollops) {
+        nav.classList.add('hide')
+    } else {
+        nav.classList.remove('hide')
+    }
+
+    prevScrollops = currentScrollops;
+})
+
+
+
+
 
 const myObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
